a3f5376c8e22e21f9bec1c163bd5313401bb9aca
Dalton Casey <daltoncasey@icloud.com>
2023-07-03 20:16:07 -0400

    Had to redo Favorites button.  Because Thumbnail itself is a link to the video player, the favorites icon would favorite the video and open the video player.  The solution was adding e.stopPropogation(); to the button handler.  Works as expected now.  The value of the favorites is tracked by isFavorite and is passed via a prop for later use

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -5,11 +5,31 @@
 import "./Thumbnail.scss";
 
 // React Imports
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
-const Thumbnail = ({ title, instructor, image, instructorImage, onClick }) => {
+const Thumbnail = ({
+    title,
+    instructor,
+    image,
+    instructorImage,
+    isFavorite,
+    onClick,
+}) => {
     const thumbnailStyle = {
         backgroundImage: `linear-gradient(-90deg, transparent, rgba(0, 0, 0, .7)), url(${image})`,
+    };
+
+    const [favorite, setFavorite] = useState(isFavorite);
+
+    const handleFavoriteToggle = () => {
+        setFavorite((prevFavorite) => !prevFavorite);
+    };
+
+    const handleButtonClicked = (e) => {
+        e.stopPropagation();
+        handleFavoriteToggle();
     };
 
     //console.log(instructorImage);
@@ -21,6 +41,17 @@ const Thumbnail = ({ title, instructor, image, instructorImage, onClick }) => {
         >
             <div className="thumbnail-content">
                 <h3 className="date-and-duration">April 01, 2023 | 53:43</h3>
+                <button
+                    className="thumbnail-favorite-button"
+                    onClick={handleButtonClicked}
+                >
+                    <FontAwesomeIcon
+                        className="heart-icon"
+                        icon={faHeart}
+                        color={favorite ? "red" : "black"}
+                        strokeWidth={favorite ? 0 : 2}
+                    />
+                </button>
                 <div className="title-and-instructor-container">
                     <h1 className="video-title">{title}</h1>
                     <h3 className="instructor-name">
