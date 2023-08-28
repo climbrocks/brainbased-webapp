8aaea9ee9e4e14700e177ff62ab9d44495615940
climbrocks <aaron@transparentwebsolutions.com>
2023-08-27 20:06:00 -0400

    favorites fixed, function properly passed to videoplayerdata, favorites working holistically

--- src/components/VideoPlayerData.js
+++ src/components/VideoPlayerData.js
@@ -76,7 +76,8 @@ const VideoPlayerData = ({
     instructor,
     isFavorite,
     favorites,
-    toggleFavorite,
+    //toggleFavorite,
+    onFavoriteToggle,
 }) => {
     /* Change tab title */
     useEffect(() => {
@@ -88,6 +89,16 @@ const VideoPlayerData = ({
         favorites ? favorites.includes(video.id) : isFavorite
     );
 
+    const handleFavoriteClick = (e) => {
+        try {
+            setFavoriteStatus(!favoriteStatus);
+            onFavoriteToggle(video.id, !isFavorite);
+        } catch (error) {
+            console.log("Error updating favorite status:", error);
+        }
+    };
+
+    /*
     const handleFavoriteClick = async () => {
         // Use the toggleFavorite function from the Home component to update favorites
         try {
@@ -97,7 +108,7 @@ const VideoPlayerData = ({
             console.log("Error updating favorite status:", error);
         }
     };
-
+    */
     const handleShareClick = () => {
         const shareUrl = `${window.location.origin}/play/${video.id}`;
 
